4a74e126ee72c7c06a85e31995ba016b0bc0514e
Wolf <wolfwings@gmail.com>
2016-09-08 12:23:00 -0600

    Converting key-value store to on-disk instead of in-memory format.

--- keyvalue.js
+++ keyvalue.js
@@ -1,19 +1,91 @@
-global.store = {};
+// Minimalistic filesystem-based key-value store
+// Longer-term migrating this to memcache would improve matters greatly
+// However 'sync' network I/O on node is nigh-impossible
+// And the callback-hell that results would drive me to just write this
+// entire application in NASM first.
 
-exports.set = (key, value) => {
-	global.store[key] = value;
-};
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
 
-exports.get = (key) => {
-	if (global.store.hasOwnProperty(key)) {
-		return store[key];
-	} else {
-		return null;
+var stats;
+try {
+	stats = fs.statSync('./keyvalue');
+} catch (err) {
+	stats = err;
+}
+
+if ((stats instanceof Error) &&
+    (stats.code === 'ENOENT')) {
+	fs.mkdirSync('./keyvalue');
+	stats = fs.statSync('./keyvalue');
+} else if (stats instanceof Error) {
+	throw stats;
+} else if (stats.isDirectory() === false) {
+	throw Error('keyvalue exists, but is not a directory!');
+}
+
+var purged = false;
+function exitHandler(options, err) {
+	if (purged === false) {
+		purged = true;
+		if (err) console.log(err.stack);
+		console.log('Purging keyvalues');
+		fs.readdirSync('./keyvalue/').forEach((file) => {
+//			console.log(file);
+			fs.unlinkSync('./keyvalue/' + file);
+		});
+		fs.rmdirSync('./keyvalue');
+		if (options.exit) process.exit();
 	}
 }
 
-exports.delete = (key) => {
-	if (global.store.hasOwnProperty(key)) {
-		delete global.store[key];
+process.on('exit', exitHandler.bind(null, {cleanup: true}));
+process.on('SIGINT', exitHandler.bind(null, {exit: true}));
+process.on('uncaughtException', exitHandler.bind(null, {exit: true}));
+
+// Prevents filesystem-specific filename-based attacks
+function safeKey(key) {
+	var digest;
+	var hash = crypto.createHash('sha256');
+	hash.update(key);
+	digest = hash.digest('hex');
+//	console.log(key + ' ~= ' + digest);
+	return digest;
+};
+
+exports.set = (key, value) => {
+	var trueKey = safeKey(key);
+	// Unsafe to use fs.writeFile without waiting for the callback
+	// So we're stuck with the sync version for this use-case
+//	console.log(key + ' => ' + value);
+	try {
+		fs.writeFileSync('./keyvalue/' + trueKey, value);
+	} catch (err) {
+		return;
 	}
+};
+
+exports.get = (key) => {
+	var value = null;
+	var trueKey = safeKey(key);
+//	console.log(key + ' <=');
+	try {
+		value = fs.readFileSync('./keyvalue/' + trueKey).toString('utf8');
+	} catch (err) {
+//		console.log(err.stacktrace);
+//		console.log(err.message);
+		value = null;
+	}
+//	console.log(value);
+	return value;
+}
+
+exports.delete = (key) => {
+	var trueKey = safeKey(key);
+//	console.log('[' + key + ']');
+	// THIS we can do async at least, huzzah!
+	fs.unlink('./keyvalue/' + trueKey, (err) => {
+		return;
+	});
 }
