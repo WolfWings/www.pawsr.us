1d4795be72492bee5affc1df9e58eb4106e7424f
Wolf <wolfwings@gmail.com>
2016-09-10 10:49:05 -0600

    Adding 'logout' URL for twitter
    This is forward-looking, to allow users to add multiple accounts
    more easily from third-party services by providing a direct
    'logout' link for each service when possible.

--- routes/service_twitter.js
+++ routes/service_twitter.js
@@ -3,5 +3,6 @@ exports.register = (endpoints, shared_data) => {
 	shared_data.services.push({
 		name:		'Twitter'
 	,	login_url:	'/initlogin/twitter'
+	,	logout_uti:	'https://twitter.com/logout'
 	});
 }
