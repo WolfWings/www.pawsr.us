23d73a4f72f1dc938bcad298ca7b10c21fbe73e8
Wolf <wolfwings@gmail.com>
2016-09-24 03:04:11 -0600

    Parameterizing OAuth2 service_*_login

--- routes/service_amazon_login.js
+++ routes/service_amazon_login.js
@@ -16,125 +16,17 @@ exports.register = (endpoints, shared_data) => {
 
 
 
-try {
-	if (data.query.state !== data.session[service + '_uuid']) {
-		throw Error('Nonce/State Mismatch - CSRF attack?');
-	}
-	if (typeof data.query.code === 'undefined') {
-		throw Error('OAuth Code missing!');
-	}
-} catch (err) {
-	console.log(serviceTitle + ': ' + err.message);
-	console.log(err.stacktrack);
-
-	delete(data.session[service + '_uuid']);
-
-	res.saveSession(data.session);
-	res.write(data.boilerplate.pretitle);
-	res.write('<title>' + serviceTitle + ' Login Callback - www.pawsr.us</title>');
-	res.write(data.boilerplate.prebody);
-	res.write('<p><b>Error:</b> ' + serviceTitle + ' did not successfully login.</p>');
-	res.write('<p><a href=\x22/\x22>Click here to go back to the homepage, and try again later.</a></p>');
-	res.write(data.boilerplate.postbody);
-	return;
-}
-
-var uuid = 'login_' + service + '_' + data.session[service + '_uuid'];
-keyvalue.set(uuid, 'wip');
-
-res.statusCode = 307;
-res.saveSession(data.session);
-res.setHeader('Location', '/login');
-res.end();
-
-var params = querystring.stringify({
-	client_id:		secrets.clientID
-,	client_secret:		secrets.clientSecret
-,	code:			data.query.code
-,	grant_type:		'authorization_code'
-,	redirect_uri:		'https:\x2F/www.pawsr.us/login/' + service
-}, '&', '=', {encodeURIComponent: x => x});
-
-var url = {
-	method: 'POST'
-,	protocol: 'https:'
-,	port: 443
-,	agent: false
-,	host: 'api.amazon.com'
-,	path: '/auth/o2/token'
-
-,	headers: {
-		'User-Agent': data.user_agent
-	,	'Accept': 'application\x2Fjson'
-	,	'Content-Type': 'application/x-www-form-urlencoded'
-	,	'Content-Length': Buffer.byteLength(params)
-	}
-};
-
-var request = https.request(url, (response) => {
-	var buffer = Buffer.alloc(0);
-	response.setEncoding('utf8');
-	response.on('data', (chunk) => {
-		buffer = Buffer.concat([buffer, Buffer.from(chunk, 'utf8')]);
-	});
-	/* istanbul ignore next: No sane way to store valid creds, remainder is straight-forward */
-	response.on('end', () => {
-		if (response.statusCode !== 200) {
-			keyvalue.set(uuid, 'error:' + response.statusCode);
-			return;
-		}
-
-		var results = JSON.parse(buffer.toString('utf8'));
-		if (typeof results.access_token === 'undefined') {
-			keyvalue.set(uuid, 'error:No access_token returned');
-			return;
-		}
-
-		url = {
-			method: 'GET'
-		,	protocol: 'https:'
-		,	port: 443
-		,	agent: false
-		,	host: 'api.amazon.com'
-		,	path: '/user/profile'
-		,	headers: {
-				'User-Agent': data.user_agent
-			,	'Accept': 'application\x2Fjson'
-			,	'Authorization': 'Bearer ' + results.access_token
-			}
-		};
-
-		var request = https.request(url, (response) => {
-			var buffer = Buffer.alloc(0);
-			response.setEncoding('utf8');
-			response.on('data', (chunk) => {
-				buffer = Buffer.concat([buffer, Buffer.from(chunk, 'utf8')]);
-			});
-			response.on('end', () => {
-				if (response.statusCode !== 200) {
-					keyvalue.set(uuid, 'error:' + response.statusCode);
-					return;
-				}
-
-				var results = JSON.parse(buffer.toString('utf8'));
-
-				require('../utils/login_complete.js')(data.session.userid, serviceTitle, uuid, results.user_id, results.name);
-			});
-		});
-		request.on('error', (e) => {
-			keyvalue.set(uuid, 'error:' + serviceTitle + ' API request failure.');
-			console.log(`Problem with request: ${e.message}`);
-		});
-		request.end();
-	});
-});
-/* istanbul ignore next: No way to force CURL errors */
-request.on('error', (e) => {
-	keyvalue.set(uuid, 'error:' + serviceTitle + ' API request failure.');
-	console.log(`Problem with request: ${e.message}`);
-});
-request.write(params);
-request.end();
+oauth.oauth2_login(
+	data
+,	res
+,	serviceTitle
+,	secrets
+,	'https:\x2F/api.amazon.com/auth/o2/token'
+,	null
+,	'https:\x2F/api.amazon.com/user/profile'
+,	'user_id'
+,	'name'
+);
 
 
 
--- utils/oauth.js
+++ utils/oauth.js
@@ -1,5 +1,8 @@
+const https = require('https');
 const crypto = require('crypto');
 const querystring = require('querystring');
+const _url = require('url');
+const keyvalue = require('../utils/keyvalue.js');
 
 exports.nonce = () => {
 	return crypto.randomBytes(24).toString('base64').replace(/\x2f/g, '_').replace(/\x2b/g, '-');
@@ -28,4 +31,120 @@ exports.oauth2_initlogin = (data, res, service, clientID, loginURL, scope) => {
 	,	state: uuid
 	}) + '#');
 	res.end();
+};
+
+exports.oauth2_login = (data, res, serviceTitle, secrets, a_t_url, a_t_auth, u_p_url, u_p_id, u_p_name) => {
+	const service = serviceTitle.toLowerCase();
+
+	try {
+		if (data.query.state !== data.session[service + '_uuid']) {
+			throw Error('Nonce/State Mismatch - CSRF attack?');
+		}
+		if (typeof data.query.code === 'undefined') {
+			throw Error('OAuth Code missing!');
+		}
+	} catch (err) {
+		console.log(serviceTitle + ': ' + err.message);
+		console.log(err.stacktrace);
+
+		delete(data.session[service + '_uuid']);
+
+		res.saveSession(data.session);
+		res.write(data.boilerplate.pretitle);
+		res.write('<title>' + serviceTitle + ' Login Callback - www.pawsr.us</title>');
+		res.write(data.boilerplate.prebody);
+		res.write('<p><b>Error:</b> ' + serviceTitle + ' did not successfully login.</p>');
+		res.write('<p><a href=\x22/\x22>Click here to go back to the homepage, and try again later.</a></p>');
+		res.write(data.boilerplate.postbody);
+		return;
+	}
+
+	var uuid = 'login_' + service + '_' + data.session[service + '_uuid'];
+	keyvalue.set(uuid, 'wip');
+
+	res.statusCode = 307;
+	res.saveSession(data.session);
+	res.setHeader('Location', '/login');
+	res.end();
+
+	var params = querystring.stringify({
+		client_id:		secrets.clientID
+	,	client_secret:		secrets.clientSecret
+	,	code:			data.query.code
+	,	grant_type:		'authorization_code'
+	,	redirect_uri:		'https:\x2F/www.pawsr.us/login/' + service
+	}, '&', '=', {encodeURIComponent: x => x});
+
+	var url = _url.parse(a_t_url, false, true);
+	url.method = 'POST';
+	url.agent = false;
+	if (a_t_auth !== null) {
+		url.auth = a_t_auth.clientID + ':' + a_t_auth.clientSecret;
+	}
+	url.headers = {
+		'User-Agent': data.user_agent
+	,	'Accept': 'application\x2Fjson'
+	,	'Content-Type': 'application/x-www-form-urlencoded'
+	,	'Content-Length': Buffer.byteLength(params)
+	};
+
+	var request = https.request(url, (response) => {
+		var buffer = Buffer.alloc(0);
+		response.setEncoding('utf8');
+		response.on('data', (chunk) => {
+			buffer = Buffer.concat([buffer, Buffer.from(chunk, 'utf8')]);
+		});
+		/* istanbul ignore next: No sane way to store valid creds, remainder is straight-forward */
+		response.on('end', () => {
+			if (response.statusCode !== 200) {
+				keyvalue.set(uuid, 'error:' + response.statusCode);
+				return;
+			}
+
+			var results = JSON.parse(buffer.toString('utf8'));
+			if (typeof results.access_token === 'undefined') {
+				keyvalue.set(uuid, 'error:No access_token returned');
+				return;
+			}
+
+			url = _url.parse(u_p_url);
+			url.method = 'GET';
+			url.agent = false;
+			url.headers = {
+				'User-Agent': data.user_agent
+			,	'Accept': 'application\x2Fjson'
+			,	'Authorization': 'Bearer ' + results.access_token
+			};
+
+			var request = https.request(url, (response) => {
+				var buffer = Buffer.alloc(0);
+				response.setEncoding('utf8');
+				response.on('data', (chunk) => {
+					buffer = Buffer.concat([buffer, Buffer.from(chunk, 'utf8')]);
+				});
+				response.on('end', () => {
+					if (response.statusCode !== 200) {
+						keyvalue.set(uuid, 'error:' + response.statusCode);
+						return;
+					}
+
+					var results = JSON.parse(buffer.toString('utf8'));
+
+					require('../utils/login_complete.js')(data.session.userid, serviceTitle, uuid, results[u_p_id], results[u_p_name]);
+				});
+			});
+			request.on('error', (e) => {
+				keyvalue.set(uuid, 'error:' + serviceTitle + ' API request failure.');
+				console.log(`Problem with request: ${e.message}`);
+			});
+			request.end();
+		});
+	});
+	/* istanbul ignore next: No way to force CURL errors */
+	request.on('error', (e) => {
+		keyvalue.set(uuid, 'error:' + serviceTitle + ' API request failure.');
+		console.log(`Problem with request: ${e.message}`);
+	});
+	request.write(params);
+	request.end();
 };
