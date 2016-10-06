b4cad715c5a87bf81de818352da526acaabfcfe1
Wolf <wolfwings@gmail.com>
2016-10-06 06:12:48 -0600

    Template prep for SVG sprite sheets Cleaning up the templates to centralized the footer into postbody.def and adding the first script: SVG4Everybody. This will allow using any SVG sprite sheets in a progressive-enhancement way by polyfilling for older browsers.
    Extensive CSS work still needed, and moving as much as possible async
    to avoid blocking the rendering for 'accessory' imagery.

---
+++ routes/scripts.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const svg4e = fs.readFileSync('./static/scripts/svg4everybody.js');
+
+module.exports = (endpoints, shared_data) => {
+	console.log('Registering script resources');
+	endpoints.push({
+		uri: '/svg4everybody.js'
+	,	routine: (data, res) => {
+
+
+
+res.setHeader('Content-Type', 'application/javascript');
+res.write(svg4e);
+res.end();
+return Promise.resolve();
+
+
+
+		}
+	});
+};
